e9a3a2885d3139e8ceee67729a09c35511e2d5ca
kimts2628 <tsts_@likelion.net>
2021-05-20 18:02:43 +0900

    Complete reply model Create & Read

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,14 +1,16 @@
 from todo import db
 
 class TodoList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.Text(), nullable=False)
-    create_data = db.Column(db.String(200), nullable=False)
+    __tablename__ = 'todolist'
+    id = db.Column(db.Integer, primary_key = True)
+    label = db.Column(db.String(200), nullable = False)
+    content = db.Column(db.Text(), nullable = False)
+    create_data = db.Column(db.String(200), nullable = False)
 
 class Reply(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id', ondelete='CASCADE'))
+    __tablename__ = 'reply'
+    id = db.Column(db.Integer, primary_key = True)
+    todo_id = db.Column(db.Integer, db.ForeignKey('todolist.id', ondelete='CASCADE'))
     todo = db.relationship('TodoList', backref = db.backref('reply_set'))
-    content = db.Column(db.Text(), nullable=False)
-    create_data = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable = False)
+    create_data = db.Column(db.String(200), nullable = False)
--- app/views/reply_view.py
+++ app/views/reply_view.py
@@ -6,11 +6,11 @@ from app.models.todo import TodoList, Reply
 
 bp = Blueprint('reply', __name__, url_prefix='/reply')
 
-@bp.route('/create/<int:todo_id>', methods=('POST'))
+@bp.route('/create/<int:todo_id>', methods=['POST'])
 def create(todo_id):
     todo = TodoList.query.get_or_404(todo_id)
     content = request.form['content']
-    reply = Reply(content = content, create_date = datetime.now())
+    reply = Reply(content = content, create_data = datetime.now())
     todo.reply_set.append(reply)
     db.session.commit()
     return redirect(url_for('todo.todo_detail', todo_id = todo_id))
